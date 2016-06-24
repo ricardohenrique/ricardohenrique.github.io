a4313f20b2236a09472cbc2b0c19c613e22e4f94
Ricardo Henrique <ricardo.hb@outlook.com>
2016-06-24 11:52:09 -0300

    first version english and minimum responsive

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(function(){
+    // nav
     $('#navbar-close').click(function() {
         console.log('click');
         $('.nav').removeClass('open').hide(150);
@@ -9,5 +10,19 @@ $(function(){
         } else {
             $('.nav').removeClass('open').hide(150);
         }
+    });
+
+    $('.nav-languages ul li#pt').click(function(){
+        $('.nav-languages ul li#en').removeClass('active');
+        $('.nav-languages ul li#pt').addClass('active');
+        $('#curriculo-en').hide(0);
+        $('#curriculo-pt').show(300);
+    });
+
+    $('.nav-languages ul li#en').click(function(){
+        $('.nav-languages ul li#pt').removeClass('active');
+        $('.nav-languages ul li#en').addClass('active');
+        $('#curriculo-pt').hide(0);
+        $('#curriculo-en').show(300);
     });
 });
