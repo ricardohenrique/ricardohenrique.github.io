6a3d8d7090ba6c50e5a6c6c7fa3e4d0136472e75
Ricardo Henrique <ricardo.hb@outlook.com>
2016-07-01 12:00:56 -0300

    englis version complete and portfolio started

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 $(function(){
     // nav
-    $('#navbar-close').click(function() {
-        console.log('click');
-        $('.nav').removeClass('open').hide(150);
-    });
+    $('#navbar-close').click(function() {closeMenu()});
+
     $('#navbar-click').click(function() {
         if (!$('.nav').hasClass('open')) {
-            $('.nav').addClass('open').show(150);
+            openMenu();
         } else {
-            $('.nav').removeClass('open').hide(150);
+            closeMenu();
         }
     });
 
     $('.nav-languages ul li#pt').click(function(){
         $('.nav-languages ul li#en').removeClass('active');
         $('.nav-languages ul li#pt').addClass('active');
-        $('#curriculo-en').hide(0);
-        $('#curriculo-pt').show(300);
+        $('#curriculo-en').hide('clip', 300);
+        $('#curriculo-pt').show('clip', 500);
     });
 
     $('.nav-languages ul li#en').click(function(){
         $('.nav-languages ul li#pt').removeClass('active');
         $('.nav-languages ul li#en').addClass('active');
-        $('#curriculo-pt').hide(0);
-        $('#curriculo-en').show(300);
+        $('#curriculo-pt').hide('clip', 300);
+        $('#curriculo-en').show('clip', 500);
     });
 });
+function openMenu(){
+    $(function(){
+        // console.log('click');
+        $('.nav').addClass('open').show('drop', 300);
+    });
+}
+function closeMenu(){
+    $(function(){
+        // console.log('click');
+        $('.nav').removeClass('open').hide('drop', 300);
+    });
+}
