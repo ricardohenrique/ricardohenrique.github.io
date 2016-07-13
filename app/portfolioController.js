46a4c3e1f5e941d69015ca3a59f79e1514cb4d9a
Ricardo Henrique <ricardo.hb@outlook.com>
2016-07-13 18:47:19 -0300

    images portfolio

--- app/portfolioController.js
+++ app/portfolioController.js
@@ -29,6 +29,21 @@ app.controller("portfolioController", function($scope) {
                         { id : 4, name : 'PRG Brasil', image: 'ricardo-henrique-mota-prg-brasil.jpg'},
                         { id : 5, name : 'Ocean2Share', image: 'ricardo-henrique-mota-ocean-2-share.jpg'},
                         { id : 6, name : 'LPG Brasil', image: 'ricardo-henrique-mota-lpg-mais.jpg'},
-                        { id : 7, name : 'CTM Advogados', image: 'ricardo-henrique-mota-ctm-advogados.jpg'}
+                        { id : 7, name : 'CTM Advogados', image: 'ricardo-henrique-mota-ctm-advogados.jpg'},
+                        { id : 8, name : 'Alphamesh', image: 'ricardo-henrique-mota-alphamesh.jpg'},
+                        { id : 9, name : 'Pratsy', image: 'ricardo-henrique-mota-pratsy.jpg'},
+                        { id : 10, name : 'Bar2Go', image: 'ricardo-henrique-mota-bar2go.jpg'},
+                        { id : 11, name : 'Balsalobre', image: 'ricardo-henrique-mota-balsalobre.jpg'},
+                        { id : 12, name : 'GIZE', image: 'ricardo-henrique-mota-gize.jpg'},
+                        { id : 13, name : 'Maadri', image: 'ricardo-henrique-mota-maadri.jpg'},
+                        // { id : 14, name : 'MAJESTIC', image: 'ricardo-henrique-sem-imagem.jpg'},
+                        { id : 15, name : 'PDVnet', image: 'ricardo-henrique-mota-pdv-net.jpg'},
+                        { id : 16, name : 'Regis Folco', image: 'ricardo-henrique-mota-regis-folco.jpg'},
+                        { id : 17, name : 'Cia de Comédia', image: 'ricardo-henrique-mota-cia-de-comedia.jpg'},
+                        { id : 18, name : 'SER GLASS', image: 'ricardo-henrique-mota-ser-glass.jpg'},
+                        { id : 19, name : 'Totality', image: 'ricardo-henrique-mota-totality.jpg'},
+                        { id : 20, name : 'SINPAPEL', image: 'ricardo-henrique-mota-sinpapel.jpg'},
+                        { id : 21, name : 'Uchinanchu', image: 'ricardo-henrique-mota-uchinanchu.jpg'},
+                        { id : 22, name : 'Você Brasil', image: 'ricardo-henrique-mota-voce-brasil.jpg'}
                     ];
 });
