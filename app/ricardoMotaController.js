15e23228ee5f66b8bc4f779968ec056add5299a2
Ricardo Mota <ricardo.mota@ftd.com.br>
2018-12-14 14:34:27 -0200

    updated info

--- app/ricardoMotaController.js
+++ app/ricardoMotaController.js
@@ -13,7 +13,8 @@ app.controller("ricardoMotaController", function($scope) {
                 $scope.lastName;
     };
     $scope.position = "Full Stack Developer";
-    $scope.apresentation = 'Acredito que o bom profissional é aquele que sempre está atualizado com as novas tecnologias, além de procurar pela aprendizagem constante.<br>Posso destacar algumas habilidades para a área como: experiência de 2 anos na área atuando com desenvolvimento server-side e client-side, vivência internacional, além de ter conhecimentos em tecnologias atuais como AngularJS, Pré-processadores(SASS e LESS), Restful, metodologias ágeis(Scrum, Kanban) entre outros.<br>Posso destacar de minhas características pessoais: assertividade, proatividade, objetividade e transparência.<br>';
+    // $scope.apresentation = 'Acredito que o bom profissional é aquele que sempre está atualizado com as novas tecnologias, além de procurar pela aprendizagem constante.<br>Posso destacar algumas habilidades para a área como: experiência de 2 anos na área atuando com desenvolvimento server-side e client-side, vivência internacional, além de ter conhecimentos em tecnologias atuais como AngularJS, Pré-processadores(SASS e LESS), Restful, metodologias ágeis(Scrum, Kanban) entre outros.<br>Posso destacar de minhas características pessoais: assertividade, proatividade, objetividade e transparência.<br>';
+    $scope.apresentation = '<p>Tenho trabalhado com desenvolvimento desde 2010.<br>Minhas principais habilidades são:<br>Back-end: PHP, Laravel, Codeigniter, Desenvolvimento e Arquitetura de API, TDD, phpunit;<br>Front-end: HTML, CSS, JavaScript, Jquery, TypeScript, Angular, Bootstrap;<br>Database, infra e outros: MySQL, SQL Server, MongoDB, GIT, NgInx, Vagrant.</p>'
 
     // Links redes sociais
     $scope.links = [    { id : 'github', link : 'https://github.com/ricardohenrique'},
